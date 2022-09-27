9dff84352bd38dc270c01b143d3b1bae73147589
nickolastazes <nickolas@readnet.gr>
2022-09-28 02:04:49 +0300

    i lied

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -5,14 +5,14 @@ type Props = {}
 function Header({}: Props) {
   return (
     <div className='flex flex-col mx-auto py-5'>
-      <div className="relative px-4 sm:px-4 lg:px-8 justify-between">
-          <nav className='max-w-2xl flex mx-auto items-center justify-between'>
+      <div className="relative px-4 sm:px-4 lg:px-8">
+          <nav className='max-w-2xl flex mx-auto items-center justify-evenly'>
 
-            <a href='/' className='text-lg font-semibold text-slate-100 block '>
+            <a href='/' className='text-lg flex font-semibold text-slate-100'>
             <span>Nickolas Tazes</span>
             </a>
-            <div className='text-base space-x-4 text-slate-100 justify-between'>
-            <a href="/blog" className='hover:text-orange-500'>Blog</a>
+            <div className='text-base flex  text-slate-100'>
+            <a href="/blog" className='hover:text-orange-500 mr-4'>Blog</a>
             <a href="mailto:n@tazes.me" className='hover:text-orange-500'>Contact</a>
             </div>
             
