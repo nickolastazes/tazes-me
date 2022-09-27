b8b50bc68d53445259f550609008bc76be5d1927
nickolastazes <nickolas@readnet.gr>
2022-09-28 00:55:14 +0300

    gamw to spiti sou 2

--- Components/BackBtn.tsx
+++ Components/BackBtn.tsx
@@ -5,8 +5,8 @@ type Props = {}
 
 function BackBtn({}: Props) {
   return (
-    <div className='flex text-sm items-center space-x-1 text-slate-100 rounded-lg border border-zinc-600 py-1 pr-2.5 pl-1 hover:border-orange-700'>
-        <a href="/"> <ChevronLeftIcon className='flex h-4 w-4'/>Back</a>
+    <div className='backbtn'>
+        <a href="/" className='backbtn'> <ChevronLeftIcon className='h-4 w-4'/>Back</a>
     </div>
   )
 }
