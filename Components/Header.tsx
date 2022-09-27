912c4c9aa4e96f27419d137c7a5afa5543133b03
nickolastazes <nickolas@readnet.gr>
2022-09-27 23:50:46 +0300

    some more changes

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -24,7 +24,6 @@ function Header({}: Props) {
                     </a>
                     <div className="-mr-2 flex items-center md:hidden">
                       <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-300 hover:text-orange-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
-                        <span className="sr-only">Open main menu</span>
                         <Bars3BottomRightIcon className="h-7 w-7" aria-hidden="true" />
                       </Popover.Button>
                     </div>
