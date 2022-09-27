d2513db767c1db0eee4505a93d6818adf8d014b8
nickolastazes <nickolas@readnet.gr>
2022-09-28 00:30:15 +0300

    changes so nav can work

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -14,7 +14,7 @@ const navigation = [
 function Header({}: Props) {
   return (
     <div className='py-5 sticky top-0 z-10 navbar'>
-                  <Popover>
+            
             <div className="mx-auto max-w-2xl px-4 md:px-0.5 lg:px-0.5">
               <nav className="relative flex items-center justify-between sm:h-10 md:justify-center" aria-label="Global">
                 <div className="flex flex-1 items-center md:absolute md:inset-y-0 md:left-0">
@@ -22,15 +22,10 @@ function Header({}: Props) {
                     <a href="/">
                     <h1 className="block text-left text-slate-100 font-semibold tracking-wide">Nickolas Tazes</h1>
                     </a>
-                    <div className="-mr-2 flex items-center md:hidden">
-                      <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-300 hover:text-orange-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3BottomRightIcon className="h-7 w-7" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
+
                   </div>
                 </div>
-                <div className="hidden md:flex md:space-x-10 md:absolute md:inset-y-0 md:right-0 md:items-center md:justify-end">
+                <div className="md:flex space-x-3 md:space-x-10 md:absolute md:inset-y-0 md:right-0 md:items-center md:justify-end">
                   {navigation.map((item) => (
                     <a key={item.name} href={item.href} className="text-slate-100 hover:text-orange-700">
                       {item.name}
@@ -39,44 +34,6 @@ function Header({}: Props) {
                 </div>
               </nav>
             </div>
-
-            <Transition
-              as={Fragment}
-              enter="duration-150 ease-out"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="duration-100 ease-in"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Popover.Panel
-                focus
-                className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
-              >
-                <div className="overflow-hidden rounded-lg bg-[#191919] shadow-md ring-1 ring-black ring-opacity-5">
-                  <div className="flex items-center justify-end px-5 pt-4">
-                    <div className="-mr-2">
-                      <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-300 hover:text-orange-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-300">
-                        <span className="sr-only">Close main menu</span>
-                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                  <div className="space-y-2 text-center px-2 pt-2 pb-3">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="block rounded-md px-3 py-3 text-base font-medium text-gray-300 hover:bg-gray-50 hover:text-gray-900"
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
     </div>
   )
 }
