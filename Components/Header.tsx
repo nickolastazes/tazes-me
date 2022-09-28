0c62fe4fac1f85829c688da101d40673131934cd
nickolastazes <nickolas@readnet.gr>
2022-09-28 16:16:56 +0300

    test

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -4,12 +4,12 @@ type Props = {}
 
 function Header({}: Props) {
   return (
-    <div className='flex flex-col mx-auto py-5'>
+    <div className='flex flex-col mx-auto py-6'>
       <div className="relative px-4 sm:px-4 lg:px-8">
           <nav className='max-w-2xl flex mx-auto items-center justify-between'>
 
             <a href='/' className='text-lg flex font-semibold text-slate-100'>
-            <span>Nickolas Tazes</span>
+            <span>Website</span>
             </a>
             <div className='text-base flex text-orange-500 space-x-4'>
             <a href="/blog" className='hover:text-orange-700'>Blog</a>
